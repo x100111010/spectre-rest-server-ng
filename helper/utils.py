8cb8184e012b1ec2315f64333c2dfe60c6ded80f
x100111010 <167847953+x100111010@users.noreply.github.com>
2025-05-24 16:44:54 +0200

    cache control based on abs value

--- helper/utils.py
+++ helper/utils.py
@@ -6,20 +6,23 @@ from endpoints.get_virtual_chain_blue_score import current_blue_score_data
 
 def add_cache_control(blue_score, timestamp, response):
     if blue_score and current_blue_score_data["blue_score"] > 0:
-        if int(blue_score) > current_blue_score_data["blue_score"] - 20 * BPS:
-            response.headers["Cache-Control"] = "public, max-age=2"
-        elif int(blue_score) > current_blue_score_data["blue_score"] - 60 * BPS:
-            response.headers["Cache-Control"] = "public, max-age=10"
-        elif int(blue_score) > current_blue_score_data["blue_score"] - 600 * BPS:
-            response.headers["Cache-Control"] = "public, max-age=60"
-        else:
-            response.headers["Cache-Control"] = "public, max-age=600"
+        delta_seconds = (
+            abs(current_blue_score_data["blue_score"] - int(blue_score)) / BPS
+        )
     elif timestamp:
-        if int(timestamp) / 1000 > int(time.time()) - 20:
-            response.headers["Cache-Control"] = "public, max-age=2"
-        elif int(timestamp) / 1000 > int(time.time()) - 60:
-            response.headers["Cache-Control"] = "public, max-age=10"
-        elif int(timestamp) / 1000 > int(time.time()) - 600:
-            response.headers["Cache-Control"] = "public, max-age=60"
-        else:
-            response.headers["Cache-Control"] = "public, max-age=600"
+        delta_seconds = abs(time.time() - int(timestamp) / 1000)
+    else:
+        return
+    if delta_seconds < 20:
+        ttl = 4
+    elif delta_seconds < 60:
+        ttl = 20
+    elif delta_seconds < 600:
+        ttl = 60
+    elif delta_seconds < 3600:
+        ttl = 600
+    elif delta_seconds < 86400:
+        ttl = 1200
+    else:
+        ttl = 3600
+    response.headers["Cache-Control"] = f"public, max-age={ttl}"
